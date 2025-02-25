2bc78bddd439e463d43c46566b80103a773137e6
nikitt <nikittaosipov@yandex.ru>
2025-02-25 20:33:03 +0400

    admin

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import NetworkNode, Product
 
-# Register your models here.
+
+@admin.action(description='Очистить задолженность перед поставщиком')
+def clear_debt(modeladmin, request, queryset):
+    queryset.update(debt=0)
+
+
+class ProductInline(admin.TabularInline):
+    model = Product
+    extra = 1
+
+
+@admin.register(NetworkNode)
+class NetworkNodeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'country', 'supplier', 'debt')
+    list_filter = ('city',)
+    search_fields = ('name', 'city')
+    inlines = [ProductInline]
+    actions = [clear_debt]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'model', 'release_date', 'network_node')
+
