44bf135b8ad6f181b5e3fb8e27598694ecd433bc
nikitt <nikittaosipov@yandex.ru>
2025-02-26 21:09:43 +0400

    views

--- network/views.py
+++ network/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import NetworkNode, Product
+from .serializers import NetworkNodeSerializer, ProductSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class IsActiveEmployee(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated and request.user.is_active
+
+
+class NetworkNodeViewSet(viewsets.ModelViewSet):
+    queryset = NetworkNode.objects.all()
+    serializer_class = NetworkNodeSerializer
+    permission_classes = [IsAuthenticated, IsActiveEmployee]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['country']
+    search_fields = ['name']
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsActiveEmployee]
